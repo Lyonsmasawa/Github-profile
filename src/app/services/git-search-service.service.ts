305177d9bec822648c4708088cfb7e8d6ce6376e
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-02-13 05:43:29 -0800

    add profile url

--- src/app/services/git-search-service.service.ts
+++ src/app/services/git-search-service.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitSearchServiceService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) {}
+    /**
+     * locateProfile
+     */
+    public locateProfile(searchQuery: String) {
+      let profileLink = `https://api.github.com/users/${searchQuery}`
+    }
+
 }
