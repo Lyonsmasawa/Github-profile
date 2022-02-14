eda48c9302b6cc6a3eb8a8da5365c2248925f171
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-02-13 22:29:45 -0800

    add functions to get user data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from './search.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-
+  //use these variables to store data collect3ed when from is submited
   public userSearch!: string
-  
-  constructor() {
+  public userProfile: any;
+  public userRepository: any[] = [];
+  public errorMessage: string | any;
+
+
+  constructor(private searchService: SearchService) { //link to service
 
   }
 
+  //fetch data
   public queryUser(){
+    this.searchService.getUserData(this.userSearch).subscribe((data) => {
+      this.userProfile = data
+    } , (error) => {
+      this.errorMessage = error;
+    });
 
+    this.searchService.getRepoData(this.userSearch).subscribe((data) => {
+      this.userRepository = data
+    } , (error) => {
+      this.errorMessage = error;
+    });
   }
+
 
   ngOnInit(): void{
 
