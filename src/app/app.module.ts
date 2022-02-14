c5a3f79597df488a376cf571a755ceb76942df90
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-02-14 00:59:36 -0800

    connect components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from "@angular/common/http"
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { RepoComponent } from './repo/repo.component';
+import { UserDataComponent } from './data/user-data/user-data.component';
+import { UserProfComponent } from './data/user-prof/user-prof.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserComponent,
+    RepoComponent,
+    UserDataComponent,
+    UserProfComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -12,7 +12,7 @@ export class SearchService {
 
   //profile search
   public getUserData(userSearch: string):Observable<any>{//brings back objects
-    let dataUrl = `https://api.github.com/users/${userSearch}?access_token=${apiKey}`;
+    let dataUrl = `https://api.github.com/users/${userSearch}?token=${apiKey}`;
     return this.httpClient.get<any>(dataUrl).pipe(
       retry(2),
       catchError(this.errorHandling)
@@ -21,7 +21,7 @@ export class SearchService {
 
   //repo search
   public getRepoData(userSearch: string):Observable<any[]>{//repos get arrays
-    let dataUrl = `https://api.github.com/users/${userSearch}/repos?access_token=${apiKey}`;
+    let dataUrl = `https://api.github.com/users/${userSearch}/repos?token=${apiKey}`;
     return this.httpClient.get<any[]>(dataUrl).pipe(
       retry(2),
       catchError(this.errorHandling)
